af079844bcd5050e3cfdffb7b0095b7bf03d2a77
Juan Goyeneche <80859255+JuanGoye15@users.noreply.github.com>
2023-07-27 01:39:41 +0000

    final

--- src/models.py
+++ src/models.py
@@ -11,24 +11,24 @@ class User(Base):
     __tablename__ = 'User'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     last_name = Column(String(250), nullable=False)
-    user_age = Column(Integer, ForeignKey('user.age'))
-    email = Column(String(250), nullable=False)    
-    id = relationship("Favorites")
+    user_age = Column(Integer)
+    email = Column(String(250), nullable=False)
+    favorites = relationship("Favorites")
 
 class Planets(Base):
     __tablename__ = 'Planets'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, ForeignKey('planets.id'))
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     climate = Column(String(250), nullable=False)
     terrain = Column(String(250), nullable=False)
     diameter = Column(Integer)
     gravity = Column(Integer)
-    id = relationship("Favorites")
+    favorites = relationship("Favorites")
     
 
 
@@ -36,32 +36,32 @@ class Characters(Base):
     __tablename__ = 'Characters'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     birth_year = Column(Integer)
     height = Column(Integer)
     gender = Column(String(250), nullable=False)
-    id = relationship("Favorites")
+    Favorites = relationship("Favorites")
 
 class Species(Base):
     __tablename__ = 'Species'
-    id = Column(Integer, ForeignKey('species.id'))
+    id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     classification = Column(String(250), nullable=False)
     average_height = Column(Integer)
     language = Column(String(250), nullable=False)
     average_lifespan = Column(Integer)
-    id = relationship("Favorites")
+    favorites = relationship("Favorites")
 
 class Favorites(Base):
     __tablename__ = 'Favorites'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    id_users = Column(Integer, ForeignKey('users.id'))
-    id_planets = Column(Integer, ForeignKey('planets.id'))
-    id_characters = Column(Integer, ForeignKey('characters.id'))
-    id = relationship("Favorites")
+    id_users = Column(Integer, ForeignKey('User.id'))
+    id_planets = Column(Integer, ForeignKey('Planets.id'))
+    id_characters = Column(Integer, ForeignKey('Characters.id'))
+    id_species = Column(Integer, ForeignKey('Species.id'))
 
     def to_dict(self):
         return {}
